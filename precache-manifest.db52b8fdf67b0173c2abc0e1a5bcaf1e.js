10da469cf3deb1b44e745c9dec6262e1882d6ce7
hai14894 <hohoanghai14894@gmail.com>
2020-10-06 14:56:38 +1100

    Updates

---
+++ precache-manifest.db52b8fdf67b0173c2abc0e1a5bcaf1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d85a8ef309bc171a091918e139bbba",
+    "url": "/Framer-Motion-PizzaJoint/index.html"
+  },
+  {
+    "revision": "ee24da05284c1763f37a",
+    "url": "/Framer-Motion-PizzaJoint/static/css/main.8b3b044d.chunk.css"
+  },
+  {
+    "revision": "ce31182f2b92b3504c10",
+    "url": "/Framer-Motion-PizzaJoint/static/js/2.4ef840e9.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Framer-Motion-PizzaJoint/static/js/2.4ef840e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee24da05284c1763f37a",
+    "url": "/Framer-Motion-PizzaJoint/static/js/main.3ae6d5e2.chunk.js"
+  },
+  {
+    "revision": "820d884c00d5a8f821fc",
+    "url": "/Framer-Motion-PizzaJoint/static/js/runtime-main.40d10bd9.js"
+  }
+]);
