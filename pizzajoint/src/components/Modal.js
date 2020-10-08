ff00577bcfce13b0630f9227b107611625a8f904
hai14894 <hohoanghai14894@gmail.com>
2020-10-08 22:24:01 +1100

    add animation for modal

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -1,10 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
 const backdrop = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
+};
+
+const modal = {
+  hidden: { opacity: 0, y: "-100vh" },
+  visible: { opacity: 1, y: "10vh",
+    transition: { delay: 0.5 } },
 };
 
 const Modal = ({ showModal, setShowModal }) => {
@@ -17,12 +23,14 @@ const Modal = ({ showModal, setShowModal }) => {
           initial="hidden"
           animate="visible"
         >
-            <motion.div className="modal">
-                <p>Want to make another pizza?</p>
-                <Link to="/">
-                    <button>Start again</button>
-                </Link>
-            </motion.div>
+          <motion.div className="modal" 
+            variants={modal}
+            >
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start again</button>
+            </Link>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
